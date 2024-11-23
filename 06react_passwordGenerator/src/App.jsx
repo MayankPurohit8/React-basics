73f06c12dc9bfc1a1bcc95c6e405ce7f9b3cedfc
MayankPurohit8 <mayankpurohit888@gmail.com>
2024-11-23 13:40:41 +0530

    commit-2-projects

--- 06react_passwordGenerator/src/App.jsx
+++ 06react_passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -9,6 +9,7 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
+  const passwordRef = useRef(null);
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -22,54 +23,83 @@ function App() {
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, password]);
+
+  const copytoclipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
   return (
     <>
-      <div className="flex flex-col justify-center bg-red-200 m-5">
-        <h1 className="text-3xl py-2">PASSWORD GENERATOR</h1>
-        <input
-          type="text"
-          className="w-2/5"
-          value={password}
-          name
-          placeholder="password"
-          readOnly
-        />
-        <div className="">
-          <input
-            type="range"
-            min={1}
-            max={100}
-            value={length}
-            onChange={(e) => {
-              setLength(Number(e.target.value));
-            }}
-          />
-          <label>length: {length}</label>
-          <input
-            type="checkbox"
-            name=""
-            id=""
-            onChange={(e) => {
-              setCharAllowed(e.target.checked);
-            }}
-          />
-          char
-          <input
-            type="checkbox"
-            name=""
-            id=""
-            onChange={(e) => {
-              setnumberAllowed(e.target.checked);
-            }}
-          />
-          number
-          <input
-            type="submit"
-            className="bg-white"
-            onClick={() => {
-              passwordGenerator();
-            }}
-          />
+      <div className="w-screen h-screen bg-[#233142] flex flex-col justify-center items-center  ">
+        <div className="bg-[#e3e3e3] p-5 border flex flex-col items-center rounded-xl shadow-white shadow-lg border-solid  border-black">
+          <h1 className="text-5xl p-3 text-[#233142]">PASSWORD GENERATOR</h1>
+          <div className="">
+            <input
+              type="text"
+              className="m-1 p-1 rounded-lg"
+              value={password}
+              name
+              placeholder="password"
+            />
+            <input
+              type="submit"
+              className="m-1 p-1  rounded-full bg-[red] text-[#e3e3e3]"
+              onClick={() => {
+                passwordGenerator();
+              }}
+            />
+            <button
+              className="m-1 p-1  rounded-full bg-[#233142] text-[#e3e3e3]"
+              onClick={() => {
+                copytoclipboard();
+              }}
+            >
+              Copy
+            </button>
+          </div>
+          <div className="h-10  w-full flex items-center gap-4">
+            <div>
+              <input
+                className="px-2 align-middle"
+                type="range"
+                min={1}
+                max={100}
+                value={length}
+                onChange={(e) => {
+                  setLength(Number(e.target.value));
+                }}
+              />
+              <label className="w-100 px-2 m-1">length: {length}</label>
+            </div>
+            <div>
+              <input
+                className="align-middle"
+                type="checkbox"
+                name="ch"
+                id=""
+                onChange={(e) => {
+                  setCharAllowed(e.target.checked);
+                }}
+              />
+              <label className="px-2" htmlFor="ch">
+                Include Characters
+              </label>
+            </div>
+            <div>
+              <input
+                className="align-middle"
+                type="checkbox"
+                name="num"
+                id=""
+                onChange={(e) => {
+                  setnumberAllowed(e.target.checked);
+                }}
+              />
+              <label className="px-2" htmlFor="num">
+                Include Numbers
+              </label>
+            </div>
+          </div>
         </div>
       </div>
     </>
